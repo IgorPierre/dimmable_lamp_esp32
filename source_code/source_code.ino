775c2331cff079c5e8e89c34fd8f264f90d58c12
IgorPierre <igor.pierre45@gmail.com>
2024-09-12 22:35:53 -0300

    refact: add the final cop on project

--- source_code/source_code.ino
+++ source_code/source_code.ino
@@ -1,16 +1,63 @@
-const int ldrPin = 36;
+#define pinZC 2 
+#define pinDIM 4 
+#define pinLDR 36      
+#define intervaloLeitura 1000   
+#define periodo 8333    
+
+#define luminosidadeIdeal 500   
+#define iMin 20                 
+#define iMax 90     
+#define difMin 0      
+#define difMax 200  
+
+volatile int intensidade = 0;
+unsigned long controleTempo;  
 
 void setup() {
   Serial.begin(115200);
   
-  pinMode(ldrPin, INPUT);
+  pinMode(pinDIM, OUTPUT); 
+  pinMode(pinLDR, INPUT);    
+  pinMode(pinZC, INPUT);       
+  attachInterrupt(digitalPinToInterrupt(pinZC), sinalZC, RISING); 
+
+  controleTempo = millis();
+  Serial.println("Sistema de lâmpada dimerizável iniciado com KY-018 LDR.");
 }
 
 void loop() {
-  int ldrValue = analogRead(ldrPin);
+  if (millis() - controleTempo > intervaloLeitura) {
+    Serial.println("Lendo luminosidade...");
+    
+    int luminosidade = analogRead(pinLDR);    
+    float difLuminosidade = luminosidadeIdeal - luminosidade;
+    
+    Serial.print("Luminosidade: ");
+    Serial.println(luminosidade);
+  
+    Serial.println("Calculando intensidade...");
+    
+    if (difLuminosidade > difMax) intensidade = iMax;
+    else if (difLuminosidade < difMin) intensidade = 0;
+    else {
+      intensidade = map(int(difLuminosidade), int(difMin), int(difMax), iMin, iMax);
+    }
+    
+    Serial.print("Intensidade: ");
+    Serial.println(intensidade);
 
-  Serial.print("Valor do LDR: ");
-  Serial.println(ldrValue);
+    controleTempo += intervaloLeitura;
+  }
+}
 
-  delay(500);
+void sinalZC() {
+  if (intensidade < iMin) return;
+  if (intensidade > iMax) intensidade = iMax;
+
+  int delayInt = periodo - (intensidade * (periodo / 100));
+
+  delayMicroseconds(delayInt);
+  digitalWrite(pinDIM, HIGH);    
+  delayMicroseconds(6);        
+  digitalWrite(pinDIM, LOW);    
 }
